4614ad1660864d5110641a0a093f1e228936e24d
Shubham Kumar (Product Architecture) <shubham.kumar@greyorange.com>
2022-12-02 12:04:55 +0530

    Day2 Part1

---
+++ Day2/main.erl
@@ -0,0 +1,34 @@
+-module(main).
+-export([solve/0]).
+-import(string, [substr/3]).
+
+solve() ->
+    {ok, Device} = file:open("input", [read]),
+    read_stdin(Device, 0).
+
+read_stdin(Device, Score) ->
+    
+    case io:get_line(Device, "") of
+        eof ->
+            init:stop(),
+            io:format("Your Score: ~p~n", [Score]),
+            file:close(Device);
+        "A X\n" ->
+            read_stdin(Device, Score + 1 + 3);
+        "A Y\n" ->
+            read_stdin(Device, Score + 2 + 6);
+        "A Z\n" ->
+            read_stdin(Device, Score + 3 + 0);
+        "B X\n" ->
+            read_stdin(Device, Score + 1 + 0);
+        "B Y\n" ->
+            read_stdin(Device, Score + 2 + 3);
+        "B Z\n" ->
+            read_stdin(Device, Score + 3 + 6);
+        "C X\n" ->
+            read_stdin(Device, Score + 1 + 6);
+        "C Y\n" ->
+            read_stdin(Device, Score + 2 + 0);
+        "C Z\n" ->
+            read_stdin(Device, Score + 3 + 3)
+    end.
