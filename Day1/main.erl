2c7c3a75f16097bcc45c891e22f9f6cfc55e1176
Shubham Kumar (Product Architecture) <shubham.kumar@greyorange.com>
2022-12-02 10:48:58 +0530

    Day 1 Completed

--- Day1/main.erl
+++ Day1/main.erl
@@ -6,19 +6,25 @@ hello() ->
 
 day1() ->
     {ok, Device} = file:open("input", [read]),
-    read_stdin(0, 0, Device).
+    read_stdin(0, 0, 0, 0, Device).
 
-read_stdin(Curr, Max, Device) ->
+read_stdin(Curr, Max, Smax, Tmax, Device) ->
     
     case io:get_line(Device, "") of
         eof ->
             init:stop(),
             file:close(Device),
-            io:format("~p", [Max]);
+            io:format("First Part: ~p~n", [max(Max, Curr)]),
+            io:format("Second Part: ~p~n", [max(Max+Smax+Tmax, Curr+Max+Smax)]);
         "\n" -> 
-            read_stdin(0, Max, Device);
+            if 
+                Curr > Max -> 
+                    read_stdin(0, Curr, Max, Smax, Device);
+                true ->
+                    read_stdin(0, Max, Smax, Tmax, Device)
+            end;
         Line ->
             S=re:replace(Line, "\\s+", "", [global, {return, list}]),
             I=list_to_integer(S),
-            read_stdin(Curr+I, max(Max, Curr + I), Device)
+            read_stdin(Curr+I, Max, Smax, Tmax, Device)
     end.
