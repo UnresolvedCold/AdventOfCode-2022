e65681caddf03589e108a5db4b8e0be96e61dcb4
Shubham Kumar (Product Architecture) <shubham.kumar@greyorange.com>
2022-12-02 12:08:53 +0530

    Day2 Part2

--- Day2/main.erl
+++ Day2/main.erl
@@ -14,11 +14,11 @@ read_stdin(Device, Score) ->
             io:format("Your Score: ~p~n", [Score]),
             file:close(Device);
         "A X\n" ->
-            read_stdin(Device, Score + 1 + 3);
-        "A Y\n" ->
-            read_stdin(Device, Score + 2 + 6);
-        "A Z\n" ->
             read_stdin(Device, Score + 3 + 0);
+        "A Y\n" ->
+            read_stdin(Device, Score + 1 + 3);
+        "A Z\n" ->
+            read_stdin(Device, Score + 2 + 6);
         "B X\n" ->
             read_stdin(Device, Score + 1 + 0);
         "B Y\n" ->
@@ -26,9 +26,9 @@ read_stdin(Device, Score) ->
         "B Z\n" ->
             read_stdin(Device, Score + 3 + 6);
         "C X\n" ->
-            read_stdin(Device, Score + 1 + 6);
-        "C Y\n" ->
             read_stdin(Device, Score + 2 + 0);
+        "C Y\n" ->
+            read_stdin(Device, Score + 3 + 3);
         "C Z\n" ->
-            read_stdin(Device, Score + 3 + 3)
+            read_stdin(Device, Score + 1 + 6)
     end.
